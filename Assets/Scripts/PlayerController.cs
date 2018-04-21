d5c87dd2270d1e175d582a46d2e65026aba2e136
amunim <abdulmunim2005@gmail.com>
2018-04-21 08:45:01 +0500

    Some Changes

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float MoveSpeed;
+    public float JumpVelocity;
+
+    private float horizontalAxis;
+    private Rigidbody2D rig;
+
+    // Use this for initialization
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalAxis = Input.GetAxisRaw("Horizontal");
+
+        if (horizontalAxis != 0)
+        {
+            rig.velocity = new Vector2(MoveSpeed * horizontalAxis, rig.velocity.y);
+        }
+
+        if (Input.GetAxisRaw("Jump") == 1f)
+        {
+            rig.velocity = new Vector2(rig.velocity.x, JumpVelocity);
+        }
+    }
+}
