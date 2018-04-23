db5647e733c41c32598b32af59c4f4d2b19d0e9d
CadoganPL <s160884@student.pg.gda.pl>
2018-04-23 19:57:26 +0200

    Cadogans1.unity

---
+++ Assets/Scripts/MenuUIManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUIManager : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject mainMenuPanel;
+    [SerializeField]
+    private GameObject playPanel;
+    [SerializeField]
+    private GameObject creditsPanel;
+    [SerializeField]
+    private GameObject pausePanel;
+
+
+    public void HideMenus()
+    {
+        HideMainMenuPanel();
+        HidePlayPanel();
+    }
+
+    public void HidePlayPanel () {
+        playPanel.SetActive(false);
+	}
+    public void ShowPlayPanel()
+    {
+        playPanel.SetActive(true);
+    }
+    public void ShowMainMenuPanel () {
+        mainMenuPanel.SetActive(true);
+	}
+
+    public void HideMainMenuPanel()
+    {
+        mainMenuPanel.SetActive(true);
+    }
+
+    public void HideCreditsPanel()
+    {
+         creditsPanel.SetActive(false);
+    }
+    public void ShowCreditsPanel()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void HidePausePanel()
+    {
+        pausePanel.SetActive(false);
+    }
+    public void ShowPausePanel()
+    {
+        pausePanel.SetActive(true);
+    }
+
+}
